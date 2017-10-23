108b6fdca041c8176d463f0a9a7e35684e668c1a
Matheus Gois <matheusgoois@gmail.com>
2017-10-23 17:44:01 -0200

    Commit da aps2

---
+++ src/Participante.java
@@ -0,0 +1,17 @@
+
+public class Participante {
+	private String nome;
+	private String email;
+	
+	public Participante(String nome, String email) {
+		super();
+		this.nome = nome;
+		this.email = email;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+}
